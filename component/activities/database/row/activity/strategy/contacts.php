b7e010dbd21a140cca49c0eb53445ff227cfc64f
Arunas Mazeika <arunas@timble.net>
2013-10-02 11:53:58 +0200

    re #414 Added contacts package strategy.

---
+++ component/activities/database/row/activity/strategy/contacts.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Nooku Framework - http://www.nooku.org
+ *
+ * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		git://git.assembla.com/nooku-framework.git for the canonical source repository
+ */
+
+namespace Nooku\Component\Activities;
+
+use Nooku\Library;
+
+/**
+ * Contacts Activity Database Row Strategy
+ *
+ * @author  Arunas Mazeika <https://github.com/amazeika>
+ * @package Nooku\Component\Activities
+ */
+class DatabaseRowActivityStrategyContacts extends DatabaseRowActivityStrategyDefault
+{
+    protected function _getString()
+    {
+        return '{actor} {action} {object} {name}';
+    }
+
+    protected function _setName(Library\ObjectConfig $config)
+    {
+        $this->_setTitle($config);
+    }
+}
